7cd58e41adf142fa938bc792e4da9906c44d05f5
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2025-05-20 12:12:23 +0300

    some edit

--- src/LineDrawing.cpp
+++ src/LineDrawing.cpp
@@ -46,10 +46,10 @@ void DrawLineMidPointDDA(HDC hdc, int x1, int y1, int x2, int y2, COLORREF color
 void DrawParametricLine(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c1, COLORREF c2){
     int r1 = GetRValue(c1), g1 = GetGValue(c1), b1 = GetBValue(c1);
     int r2 = GetRValue(c2), g2 = GetGValue(c2), b2 = GetBValue(c2);
-    int alpha1 = x2 - x1, alpha2 = y2 - y1,alphar = r2 - r1, alphag = g2 - g1, alphab = b2 - b1;
+    int alpha1 = x2 - x1, alpha2 = y2 - y1,alphaRed = r2 - r1, alphaGreen = g2 - g1, alphaBlue = b2 - b1;
     SetPixel(hdc, x1, y1, c1);
     double step = 1.0 / max(abs(alpha1), abs(alpha2));
     for (double t = 0; t <= 1; t += step)
-        SetPixel(hdc, round(x1 + t * alpha1), round(y1 + t * alpha2), RGB(round(r1 + t * alphar), round(g1 + t * alphag), round(b1 + t * alphab)));
+        SetPixel(hdc, round(x1 + t * alpha1), round(y1 + t * alpha2), RGB(round(r1 + t * alphaRed), round(g1 + t * alphaGreen), round(b1 + t * alphaBlue)));
     
 }
