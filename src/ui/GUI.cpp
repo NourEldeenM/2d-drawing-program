b0a63bf810f8f12574372babec394a3042e6312e
Nour Eldeen Mohamed <119622675+NOURisLIGHT@users.noreply.github.com>
2025-05-22 15:06:07 +0300

    refactor(gui): refactor gui code to simpler functions for readability

--- src/ui/GUI.cpp
+++ src/ui/GUI.cpp
@@ -15,7 +15,6 @@
 #include "../algorithms/QuadAndCubicCurvesAlgos/QuadraticCurveAlgorithm.cpp"
 #include "../algorithms/FillingAlgos/FloodFillAlgorithm.cpp"
 
-
 using namespace std;
 
 class GUI
@@ -68,9 +67,8 @@ public:
             "Clear Canvas"};
 
         for (int i = 0; i < menuOptionLabels.size(); i++)
-        {
             menuOptions.push_back({10, 60 + i * 45, 200, 40});
-        }
+        
     }
 
     ~GUI()
@@ -86,128 +84,139 @@ public:
     {
         InitWindow(screenWidth, screenHeight, "2d drawing program");
         SetTargetFPS(60);
-
         while (!WindowShouldClose())
         {
             handleInput();
             render();
         }
-
         CloseWindow();
     }
 
     void handleInput()
     {
         Vector2 mousePoint = GetMousePosition();
+        if (handleMenuButtonClick(mousePoint))
+            return;
+        if (handleMenuOptionSelection(mousePoint))
+            return;
+        handleCanvasClick(mousePoint);
+    }
 
-        // Check for menu button click
+    bool handleMenuButtonClick(const Vector2 &mousePoint)
+    {
         if (CheckCollisionPointRec(mousePoint, menuButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
             menuExpanded = !menuExpanded;
-            return;
+            return true;
         }
+        return false;
+    }
 
-        // Check for menu option selection if menu is expanded
+    bool handleMenuOptionSelection(const Vector2 &mousePoint)
+    {
         if (menuExpanded)
         {
             for (int i = 0; i < menuOptions.size(); i++)
             {
                 if (CheckCollisionPointRec(mousePoint, menuOptions[i]) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
                 {
-                    switch (i)
-                    {
-                    case 0: // Line (DDA)
-                        if (currentAlgorithm)
-                            delete currentAlgorithm;
-                        currentAlgorithm = new DDALineAlgorithm();
-                        isColoredLine = false;
-                        break;
-
-                    case 1: // Line (Bresenham)
-                        if (currentAlgorithm)
-                            delete currentAlgorithm;
-                        currentAlgorithm = new BresenhamLineAlgorithm();
-                        isColoredLine = false;
-                        break;
-
-                    case 2: // Line (Parametric)
-                        if (currentAlgorithm)
-                            delete currentAlgorithm;
-                        currentAlgorithm = new ParametricLineAlgorithm();
-                        isColoredLine = false;
-                        break;
-
-                    case 3: // Colored Line (Parametric)
-                    {
-                        if (currentAlgorithm)
-                            delete currentAlgorithm;
-                        auto paramLine = new ParametricLineAlgorithm();
-                        drawingColor.push_back(YELLOW);
-                        currentAlgorithm = paramLine;
-                        isColoredLine = true;
-                        break;
-                    }
-                    case 4: // Circle (Cartesian)
-                        if (currentAlgorithm)
-                            delete currentAlgorithm;
-                        currentAlgorithm = new CircleCartesianAlgorithm();
-                        isColoredLine = false;
-                        break;
-                    case 5: // Circle (Polar)
-                        if (currentAlgorithm)
-                            delete currentAlgorithm;
-                        currentAlgorithm = new CirclePolarAlgorithm();
-                        isColoredLine = false;
-                        break;
-                    case 6: // Circle (Polar Iterative)
-                        if (currentAlgorithm)
-                            delete currentAlgorithm;
-                        currentAlgorithm = new CirclePolarIterativeAlgorithm();
-                        isColoredLine = false;
-                        break;
-                    case 7: // Circle (MidPoint DDA)
-                        if (currentAlgorithm)
-                            delete currentAlgorithm;
-                        currentAlgorithm = new CircleMidPointDDAAlgorithm();
-                        isColoredLine = false;
-                        break;
-                    case 8: // Circle (Enhanced MidPoint DDA)
-                        if (currentAlgorithm)
-                            delete currentAlgorithm;
-                        currentAlgorithm = new CircleMidPointDDAModifiedAlgorithm();
-                        isColoredLine = false;
-                        break;
-                    case 9: // Quadratic Curve
-                        if (currentAlgorithm)
-                            delete currentAlgorithm;
-                        currentAlgorithm = new QuadraticCurveAlgorithm();
-                        isColoredLine = false;
-                        break;
-                    case 10: // Bezier Curve
-                        if (currentAlgorithm)
-                            delete currentAlgorithm;
-                        currentAlgorithm = new BezierCurveAlgorithm();
-                        isColoredLine = false;
-                        break;
-                    case 11: // Flood Fill
-                        if (currentAlgorithm)
-                            delete currentAlgorithm;
-                        drawingColor.push_back(BLUE);
-                        currentAlgorithm = new FloodFillAlgorithm();
-                        isColoredLine = false;
-                        break;
-                    case 12: // Clear Canvas
-                        inputPoints.clear();
-                        drawnShapes.clear();
-                        break;
-                    }
+                    handleMenuOptionsSelection(i);
                     menuExpanded = false;
                     selectedOption = i;
-                    return;
+                    return true;
                 }
             }
         }
+        return false;
+    }
+    
+    void handleMenuOptionsSelection(int i)
+    {
+        switch (i)
+        {
+        case 0: // Line (DDA)
+            if (currentAlgorithm)
+                delete currentAlgorithm;
+            currentAlgorithm = new DDALineAlgorithm();
+            isColoredLine = false;
+            break;
+        case 1: // Line (Bresenham)
+            if (currentAlgorithm)
+                delete currentAlgorithm;
+            currentAlgorithm = new BresenhamLineAlgorithm();
+            isColoredLine = false;
+            break;
+        case 2: // Line (Parametric)
+            if (currentAlgorithm)
+                delete currentAlgorithm;
+            currentAlgorithm = new ParametricLineAlgorithm();
+            isColoredLine = false;
+            break;
+        case 3: // Colored Line (Parametric)
+            if (currentAlgorithm)
+                delete currentAlgorithm;
+            drawingColor.push_back(YELLOW);
+            currentAlgorithm = new ParametricLineAlgorithm();
+            isColoredLine = true;
+            break;
+        case 4: // Circle (Cartesian)
+            if (currentAlgorithm)
+                delete currentAlgorithm;
+            currentAlgorithm = new CircleCartesianAlgorithm();
+            isColoredLine = false;
+            break;
+        case 5: // Circle (Polar)
+            if (currentAlgorithm)
+                delete currentAlgorithm;
+            currentAlgorithm = new CirclePolarAlgorithm();
+            isColoredLine = false;
+            break;
+        case 6: // Circle (Polar Iterative)
+            if (currentAlgorithm)
+                delete currentAlgorithm;
+            currentAlgorithm = new CirclePolarIterativeAlgorithm();
+            isColoredLine = false;
+            break;
+        case 7: // Circle (MidPoint DDA)
+            if (currentAlgorithm)
+                delete currentAlgorithm;
+            currentAlgorithm = new CircleMidPointDDAAlgorithm();
+            isColoredLine = false;
+            break;
+        case 8: // Circle (Enhanced MidPoint DDA)
+            if (currentAlgorithm)
+                delete currentAlgorithm;
+            currentAlgorithm = new CircleMidPointDDAModifiedAlgorithm();
+            isColoredLine = false;
+            break;
+        case 9: // Quadratic Curve
+            if (currentAlgorithm)
+                delete currentAlgorithm;
+            currentAlgorithm = new QuadraticCurveAlgorithm();
+            isColoredLine = false;
+            break;
+        case 10: // Bezier Curve
+            if (currentAlgorithm)
+                delete currentAlgorithm;
+            currentAlgorithm = new BezierCurveAlgorithm();
+            isColoredLine = false;
+            break;
+        case 11: // Flood Fill
+            if (currentAlgorithm)
+                delete currentAlgorithm;
+            drawingColor.push_back(BLUE);
+            currentAlgorithm = new FloodFillAlgorithm();
+            isColoredLine = false;
+            break;
+        case 12: // Clear Canvas
+            inputPoints.clear();
+            drawnShapes.clear();
+            break;
+        }
+    }
 
+    void handleCanvasClick(const Vector2 &mousePoint)
+    {
         if (!menuExpanded && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
             Point p = {GetMouseX(), GetMouseY()};
@@ -219,65 +228,56 @@ public:
                 {
                     vector<pair<Point, Color>> shape = currentAlgorithm->draw(inputPoints, drawingColor);
                     if (!shape.empty())
-                    {
                         drawnShapes.push_back(shape); // Store the shape
-                    }
-                    inputPoints.clear(); // Reset for next line
+                    inputPoints.clear();              // Reset for next line
                 }
             }
         }
     }
 
-    void render()
+    void render() 
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        // Draw menu button
-        DrawRectangleRec(menuButton, SKYBLUE);
-        DrawRectangleLinesEx(menuButton, 2, DARKBLUE);
-        DrawText("Menu", menuButton.x + 30, menuButton.y + 10, 20, DARKBLUE);
-
+        drawMenuButton();
         if (menuExpanded)
-        {
-            for (int i = 0; i < menuOptions.size(); i++)
-            {
-                Color bgColor = (selectedOption == i) ? LIGHTGRAY : RAYWHITE; // highlight selected option
-                DrawRectangleRec(menuOptions[i], bgColor);
-                DrawRectangleLinesEx(menuOptions[i], 1, DARKGRAY);
-                DrawText(menuOptionLabels[i], menuOptions[i].x + 10, menuOptions[i].y + 10, 20, DARKGRAY);
-            }
-        }
-
-        // Draw all stored shapes
-        for (const auto &shape : drawnShapes)
-        {
-            for (const auto &p : shape)
-            {
-                DrawPixel(p.first.x, p.first.y, p.second);
-            }
-        }
-
-        // if (isColoredLine && dynamic_cast<ParametricLineAlgorithm *>(currentAlgorithm))
-        // {
-        //     auto paramAlgorithm = dynamic_cast<ParametricLineAlgorithm *>(currentAlgorithm);
-        //     const auto &coloredPoints = paramAlgorithm->getColoredPoints();
-
-        //     if (!coloredPoints.empty() && !drawnShapes.empty())
-        //     {
-        //         for (const auto &cp : coloredPoints)
-        //         {
-        //             DrawPixel(cp.x, cp.y, Color{(unsigned char)cp.r, (unsigned char)cp.g, (unsigned char)cp.b, 255});
-        //         }
-        //     }
-        // }
-
-        // Draw current input points
-        for (const Point &p : inputPoints)
-        {
-            DrawCircle(p.x, p.y, 3, RED);
-        }
+            drawMenuOptions();
+        drawShapes();
+        drawInputPoints();
 
         EndDrawing();
     }
+    
+    void drawMenuButton() 
+    {
+        DrawRectangleRec(menuButton, SKYBLUE);
+        DrawRectangleLinesEx(menuButton, 2, DARKBLUE);
+        DrawText("Menu", menuButton.x + 30, menuButton.y + 10, 20, DARKBLUE);
+    }
+
+    void drawMenuOptions()
+    {
+        for (int i = 0; i < menuOptions.size(); i++)
+        {
+            Color bgColor = (selectedOption == i) ? LIGHTGRAY : RAYWHITE;
+            DrawRectangleRec(menuOptions[i], bgColor);
+            DrawRectangleLinesEx(menuOptions[i], 1, DARKGRAY);
+            DrawText(menuOptionLabels[i], menuOptions[i].x + 10, menuOptions[i].y + 10, 20, DARKGRAY);
+        }
+    }
+
+    void drawShapes()
+    {
+        for (const auto &shape : drawnShapes)
+            for (const auto &p : shape)
+                DrawPixel(p.first.x, p.first.y, p.second);
+    }
+
+    void drawInputPoints()
+    {
+        for (const Point &p : inputPoints)
+            DrawCircle(p.x, p.y, 3, RED);
+    }
+
 };
